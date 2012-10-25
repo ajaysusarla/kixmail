9e283153ff9f2566e2ddab9fe6d4cd3faa7b40e3
Parthasarathi Susarla <ajaysusarla@gmail.com>
2012-10-25 22:57:30 +1100

    Fix check in autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,12 +4,12 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="kixMail"
+PKG_NAME="kixmail"
 REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.ac \
   && test -f $srcdir/README \
-  && test -f $srcdir/libkix) || {
+  && test -d $srcdir/libkix) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
