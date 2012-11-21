80a3e62211c7665a345c8d26ebb5e44c224979e6
Parthasarathi Susarla <ajaysusarla@gmail.com>
2012-11-21 22:29:32 +1100

    Add gsetting schema names.

---
+++ libkix/kixmail-gsettings.h
@@ -0,0 +1,33 @@
+/*
+ * kixmail-gsettings.h
+ * Copyright (C) 2012  Parthasarathi Susarla <ajaysusarla@gmail.com>
+ *
+ * kixmail is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kixmail is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _KIXMAIL_GSETTINGS_H_
+#define _KIXMAIL_GSETTINGS_H_
+
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+#define KIXMAIL_PREFS_SCHEMA "org.gnome.Kixmail"
+
+
+#define KIXMAIL_PREFS_UI_SCHEMA KIXMAIL_PREFS_SCHEMA ".ui"
+
+G_END_DECLS
+
+#endif /*_KIXMAIL_GSETTINGS_H_*/
