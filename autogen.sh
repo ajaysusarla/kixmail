ad71847e9f9c6d529d4071525c5d40912f7d828d
Parthasarathi Susarla <ajaysusarla@gmail.com>
2013-01-02 22:23:45 +1100

    Fix automake version dependency.

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="kixmail"
-REQUIRED_AUTOMAKE_VERSION=1.9
+REQUIRED_AUTOMAKE_VERSION=1.13
 
 (test -f $srcdir/configure.ac \
   && test -f $srcdir/README \
